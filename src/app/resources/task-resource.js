8f44ff005ff9b03fe997785bfc7f2e46edce3e59
kemblekaran_ <karan.kemble@sundaymobility.com>
2020-09-01 11:59:06 +0530

    sorting & pagination support added

--- src/app/resources/task-resource.js
+++ src/app/resources/task-resource.js
@@ -24,11 +24,34 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-//fetch all task
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     try {
-        const tasks = await Task.find({ owner: req.user.id })
-        res.send(tasks)
+
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            sort,
+            options: {
+                limit: parseInt(req.query.limit), skip: parseInt(req.query.skip)
+            }
+        }).execPopulate()
+        // const tasks = await Task.find({ owner: req.user.id })
+        res.send(req.user.tasks)
     } catch (error) {
         res.status(500).send(error)
     }
